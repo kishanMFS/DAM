7e0f3b0a8ef4078582a3cf16100928c4197f26d4
kishanpp <kish101993@gmail.com>
2026-07-01 21:12:08 +0530

    feat: added frontend login test cases

--- ReactFrontend/src/pages/UserDashboard.tsx
+++ ReactFrontend/src/pages/UserDashboard.tsx
@@ -7,6 +7,8 @@ function UserDashboard() {
 
   return (
     <div>
+      <h1 className="text-3xl font-bold">User Dashboard</h1>
+
       <UploadSection onUploadComplete={() => setShouldPoll(true)} />
 
       <Gallery
